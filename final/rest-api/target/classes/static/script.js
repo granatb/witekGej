2364baf80b15a8a05dd5f2d31682ac986c0ef300
granatb <32493624+granatb@users.noreply.github.com>
2020-01-19 18:34:34 +0100

    script.js update

--- final/rest-api/target/classes/static/script.js
+++ final/rest-api/target/classes/static/script.js
@@ -1,42 +1,36 @@
-(function () {
-    var postPoint = function () {
+var postPoint = function () {
+    var loaderWait = document.getElementById("loaderWait");
+    toggle(loaderWait);
+    var pathString=$('#imageInput').val();
+    var pointJson = JSON.stringify(
+        {
+            pathString:pathString,
+            xsCoord:null,
+            ysCoord:null
+        });
+    $.ajax({
+        url         : "/api/v1/point", //wymagane, gdzie się łączymy, zw na hostowanie w tej samej aplikacji mozna uzyc sciezki wzglednej
+        method      : "post", // metoda HTTP połączenia, domyślnie get
+        contentType : 'application/json', //gdy wysyłamy dane czasami chcemy ustawić ich typ
+        data        : pointJson
+    })
+        .done(function(e){
+            clearContent();
 
-        console.log("co jest ze mna");
-        var loaderWait = document.getElementById("loaderWait");
-        toggle(loaderWait);
-        var pathString=$('#imageInput').val();
-        var pointJson = JSON.stringify(
-            {
-                pathString:pathString,
-                xsCoord:null,
-                ysCoord:null
-            });
-        $.ajax({
-            url         : "/api/v1/point", //wymagane, gdzie się łączymy, zw na hostowanie w tej samej aplikacji mozna uzyc sciezki wzglednej
-            method      : "post", // metoda HTTP połączenia, domyślnie get
-            contentType : 'application/json', //gdy wysyłamy dane czasami chcemy ustawić ich typ
-            data        : pointJson
+            refreshContent();
         })
-            .done(function(e){
-                clearContent();
+        .fail(function(e){
+            alert("Error encountered.");
+            console.log(e);
+        });
+};
 
-                refreshContent();
-            })
-            .fail(function(e){
-                alert("Error encountered.");
-                console.log(e);
-            });
-    };
-
-    window.onload = function () {
-        document.getElementById('naniesPkt').onclick = postPoint;
-        refreshContent();
-    };
-})();
+window.onload = function () {
+    document.getElementById('naniesPkt').onclick = wysylaj;
+    refreshContent();
+};
 
 var refreshContent = function () {
-    console.log("Clicked Refresh");
-
     $.ajax({
         url : "/api/v1/point",
         success : function(response) {
@@ -53,7 +47,6 @@ var refreshContent = function () {
 };
 
 var clearContent = function () {
-    console.log('Clearing content!');
     // document.getElementById('table-content-container').innerHTML = "";
     var tableHeaderObj = document.createElement('tr');
     var table = document.getElementById('table-content-container');
@@ -70,20 +63,21 @@ var clearContent = function () {
 
 };
 
+
 var clearMap = function () {
     console.log('Clearing map!');
 
     var canvas = document.getElementById("myCanvas");
     var context = canvas.getContext("2d");
     context.clearRect(0,0,canvas.width, canvas.height);
-
-
 };
+
 
 var deleteLink = function (element) {
     console.log('DELETING: ' + element.getAttribute('id'));
 
     $.ajax({
+
         url : "/api/v1/point/deleteFromArray/" + element.getAttribute('id'),
         method: 'delete',
         success : refreshContent,
@@ -168,4 +162,66 @@ var toggle = function (elem) {
         return;
     }
     show(elem);
+};
+var lista = [];
+
+var wysylaj = function(){
+    var pathString=$('#imageInput').val();
+    var i;
+    var flaga = 1;
+    if(lista[lista.length - 1] == pathString & lista.length > 1){
+        console.log("Ten pathString juz byl");
+        flaga = 0;
+    }
+    if (lista.length == 1 & lista[0] != pathString){
+        lista.push(pathString);
+        postPoint();
+    }
+    if (lista.length == 1 & lista[0] == pathString){
+        console.log("Ten pathString juz byl");
+    }
+    if (lista.length == 0){
+        lista.push(pathString);
+        postPoint();
+    }
+    if(lista.length > 1){
+        for(i = 0; i < lista.length - 1; i++){
+            if(lista[i] == pathString){
+                flaga = 0;
+                console.log("Ten pathString juz byl");
+            }
+        }
+    }
+    if(flaga == 1 & lista.length > 2){
+        lista.push(pathString);
+        postPoint();
+    }
+    if(lista[0] != pathString & lista[1] != pathString & lista.length == 2){
+        lista.push(pathString);
+        postPoint();
+    }
+};
+
+var deletePoint = function (idi) {
+    console.log('DELETING ALL');
+
+    $.ajax({
+        url : "/api/v1/point/" + idi,
+        method: 'delete',
+        success : refreshContent,
+        error : function() {},
+        complete : function() {}
+    });
+};
+
+var restartAll = function () {
+    var content_container = document.getElementById("table-content-container");
+    var numberOfDivs = content_container.getElementsByTagName('tr').length -1;
+    console.log('DELETING ALL');
+    for (i = 1; i < numberOfDivs+1; i++) {
+        deletePoint(i);
+    }
+
+    lista=[];
+    clearContent();
 };
